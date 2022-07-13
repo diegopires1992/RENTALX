0ef2016983124624670bdc854beb4b85cdd143d3
Diego Pires <diego__pires@hotmail.com>
2022-07-12 22:35:14 -0300

    feito model e repository

---
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -0,0 +1,14 @@
+import { Category } from '../model/Category';
+import { Specification } from '../model/Specification';
+
+interface ISpecificationDTO {
+  name: string;
+  description;
+}
+
+interface ISpecificationRepository {
+  findByName(name: string): Specification;
+  list(): Specification[];
+  create({ name, description }: ISpecificationDTO): void;
+}
+export { ISpecificationRepository, ISpecificationDTO };
